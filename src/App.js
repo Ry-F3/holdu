f437454b50ed1a98a9ad4c341cbf756c94ff8599
Ry-F3 <rhysfeew@gmail.com>
2024-03-26 17:10:18 +0000

    Added react routing to the index.js and App.js  files.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-
-
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
-     <NavBar />
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+        <Route exact path="/" render={() => <h1>Home Jobs Feed</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Join Now</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
-import logo from '../assets/logo.png';
-import styles from '../App.module.css';
+import React from "react";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import logo from "../assets/logo.png";
+import styles from "../App.module.css";
 
 const NavBar = () => {
   return (
@@ -9,10 +16,9 @@ const NavBar = () => {
       <Container className="d-flex align-items-center justify-content-between">
         <div className="d-flex align-items-center">
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="40" className='mr-2'/>
+            <img src={logo} alt="logo" height="40" className="mr-2" />
             <span>holdu</span>
           </Navbar.Brand>
-       
         </div>
 
         <Nav className="d-flex align-items-center">
@@ -24,13 +30,17 @@ const NavBar = () => {
           </Nav.Link> */}
           <div className="d-flex align-items-center mr-3">
             <Form className="ml-1 d-flex">
-              <FormControl type="search" placeholder="Search Jobs" className="mr-2" aria-label="Search" />
+              <FormControl
+                type="search"
+                placeholder="Search Jobs"
+                className="mr-2"
+                aria-label="Search"
+              />
               <Button variant="white" className="btn-transparent">
-              <i class="fa-solid fa-magnifying-glass"></i>
+                <i class="fa-solid fa-magnifying-glass"></i>
               </Button>
             </Form>
           </div>
-
           <div>
             <p className="mb-0 mr-2">Tagline for the app</p> {/* Tagline */}
           </div>
@@ -42,10 +52,11 @@ const NavBar = () => {
           </Nav.Link>
           <Nav.Link>
             <div className="mr-1 d-flex flex-column align-items-center">
-              <Button variant="outline-primary" className="rounded">Sign In</Button>
+              <Button variant="outline-primary" className="rounded">
+                Sign In
+              </Button>
             </div>
           </Nav.Link>
-      
         </Nav>
       </Container>
     </Navbar>
